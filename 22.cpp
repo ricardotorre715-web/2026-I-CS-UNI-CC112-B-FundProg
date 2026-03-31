592996b6b624246500bfc716a2aadf27eba9b522
Ricardo Torre <ricardotorre715@gmail.com>
2026-03-31 16:08:29 -0500

    Agregar ejercicio 22 y configurar ignore para ejecutables

---
+++ 22.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// g++ -std=c++2b main.cpp -o main
+// El main() debe ser muy pero muy pequeñito
+int main() {
+    std::cout << "Quiero ponderar fundamentos" << std::endl;
+
+    return 0;
+}
